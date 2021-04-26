de58d9a01e4b4cf07fefc179815b7e8b7a7f3be9
qishaoxuan <qishaoxuan@jd.com>
2021-04-26 15:53:30 +0800

    feat: 完成 publish 更新版本

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,34 @@
-const exia = require('../lib/index');
+#!/usr/bin/env node
+const log = require('signale');
+const semver = require('semver');
+const packageJSON = require('../package');
 
-exia();
+function checkNodeVersion(wanted, id) {
+  if (!semver.satisfies(process.version, wanted, { includePrerelease: true })) {
+    log.error(
+      `You are using Node ${process.version}, but this version of ${id} requires Node ${wanted}.\nPlease upgrade your Node version.`,
+    );
+    process.exit(1);
+  }
+}
+
+checkNodeVersion(packageJSON.engines.node, packageJSON.name);
+
+const program = require('commander');
+
+program
+  .version(`${packageJSON.name} ${require('../package').version}`)
+  .usage('<command> [options]');
+
+program
+  .command('publish')
+  .description('发布项目至npm')
+  .option('-cv, --custom-version <version>', '更新指定版本到 package.json')
+  .option('-ngc, --nogitcheck', '不检查 git 状态', false)
+  .option('-p, --prereleaseId [prereleaseId]', '发布/更新预发布版本：beta, alpha, RC ...', 'beta')
+  .option('-nt, --notag', '是否使用 git tag', false)
+  .action((options) => {
+    require('../lib/publish')(options);
+  });
+
+program.parse(process.argv);
