49501900cc32154ce92fae50919d92ceee7f309c
qishaoxuan <qishaoxuan@jd.com>
2021-04-27 17:39:28 +0800

    feat: 更换 lint 插件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: 'eslint-config-ali/node',
+  rules: {
+    'no-empty': [2, { allowEmptyCatch: true }],
+    'no-console': [0],
+    '@typescript-eslint/no-require-imports': [0],
+  },
+  extends: ['eslint-config-ali/node', 'prettier'],
 };
